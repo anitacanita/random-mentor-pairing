a84a42ee2e0344108d52159bc604d665418b9b50
ana <adscarlos@gmail.com>
2016-02-10 20:10:13 +0000

    adds script code

---
+++ mentor_mentee.rb
@@ -0,0 +1,21 @@
+require 'csv'
+
+mentor_mentee = CSV.read('mentor_mentee_feb16.csv')
+
+juniors = []
+seniors = []
+
+mentor_mentee.each_with_index do |row, i|
+  juniors << row[0..2]
+  seniors << row[3..5]
+end
+
+def random_pairs(juniors, seniors)
+  shuffled_seniors = seniors.shuffle
+  shuffled_juniors = juniors.shuffle
+  shuffled_juniors.zip(shuffled_seniors)
+end
+
+CSV.open('random_mentor_pairing.csv', 'w') do |csv|
+  random_pairs(juniors,seniors).each {|pair| csv << pair.flatten}
+end
